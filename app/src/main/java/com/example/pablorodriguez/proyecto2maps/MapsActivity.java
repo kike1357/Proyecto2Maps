5aa629be0c7f519a638a5e923e858313b8e5f744
kike1357 <quique_8_94@hotmail.com>
2017-02-03 14:02:37 +0100

    Añadida la marca en el mapa

--- app/src/main/java/com/example/pablorodriguez/proyecto2maps/MapsActivity.java
+++ app/src/main/java/com/example/pablorodriguez/proyecto2maps/MapsActivity.java
@@ -36,11 +36,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng marca = new LatLng(42.2366, -8.714552);
+        mMap.addMarker(new MarkerOptions().position(marca).title("Marca en DanielCastelao"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marca));
     }
 }
